396ef5a5968ccfa6b25d2781658f1280da6ce917
bennie12345 <bmuijldert@gmail.com>
2016-02-10 19:32:36 +0100

    fixed movement

--- Mythe_2D/Assets/Scripts/Player.cs
+++ Mythe_2D/Assets/Scripts/Player.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
+
+    private Rigidbody2D _rb;
+
+    [SerializeField]
+    private float _moveForce = 10;
 
 	// Use this for initialization
 	void Start () 
     {
-	
+        _rb = this.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
     {
-	
+        Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") * _moveForce, CrossPlatformInputManager.GetAxis("Vertical") * _moveForce);
+
+        _rb.velocity = moveVec;
 	}
 }
