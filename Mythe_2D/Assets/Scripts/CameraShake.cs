06cfb66335b25fd345346ca4a16eef4baddbc49b
bennie12345 <bmuijldert@gmail.com>
2016-02-11 17:25:48 +0100

    camera shake script added

---
+++ Mythe_2D/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour {
+    private bool _isShaking = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        CheckForShake();
+	}
+
+    public void Shake()
+    {
+        _isShaking = true;
+        StartCoroutine(CameraShaking());
+    }
+
+    void CheckForShake()
+    {
+        if (_isShaking)
+        {
+            this.gameObject.transform.position = new Vector3(Random.Range(17f, 18f), Random.Range(-14.5f, -15.5f), -59.7f);
+        }
+        else
+        {
+            this.gameObject.transform.position = new Vector3(17.3f, -15, -59.7f);
+        }
+    }
+
+    IEnumerator CameraShaking()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _isShaking = false;
+    }
+
+    //use this to shake the camera: GameObject.Find("Main Camera").GetComponent<CameraShake>().Shake();
+}
