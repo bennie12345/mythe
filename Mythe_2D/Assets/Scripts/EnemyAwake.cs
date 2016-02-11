b48335623dada1d875cd2d635f0a879957f7b669
Icedia <daniel1995@live.nl>
2016-02-11 11:09:43 +0100

    Enemy movement and death?(needs to be tested)

---
+++ Mythe_2D/Assets/Scripts/EnemyAwake.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAwake : MonoBehaviour {
+    public Animation anim;
+
+    void Start() {
+        anim = GetComponent<Animation>();
+    }
+    void Update() {
+    }
+   	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Bullet")
+			Destroy(this.gameObject);
+        
+
+	}
+
+}
+
+ 
---
+++ Mythe_2D/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+    [SerializeField] private int movespeed = 1;
+    private Vector3 userDirection = Vector3.right;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+	
+	}
+}
