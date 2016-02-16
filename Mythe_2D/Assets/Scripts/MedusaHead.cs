c8de240d2060ea85b5a102eb015332ec95ba1eeb
Lorenzo <18927@ma-web.nl>
2016-02-16 11:05:27 +0100

    Medusa stuff
    :+1:

---
+++ Mythe_2D/Assets/Scripts/MedusaHead.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MedusaHead : MonoBehaviour {
+
+    [SerializeField]private GameObject effectCollider;
+    [SerializeField]private ParticleSystem particles;
+    [SerializeField]private Transform parentObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnMouseDown();
+	}
+
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ParticleSystem instantiatedObject = Instantiate(particles, transform.position, Quaternion.Euler(0, 90, -90)) as ParticleSystem;
+            instantiatedObject.transform.parent = parentObject;
+            Instantiate(effectCollider);
+         }
+    }
+}
---
+++ SaveLoadResources/Assets/MenuItems.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MenuItems : MonoBehaviour
+{
+    [MenuItem ("Dikke bmw/Clear PlayerPrefs")]
+    public static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("player prefs cleared");
+    }
+}
