4a3cecc2ee9cdec1745e9be997327486192b1ee6
bennie12345 <bmuijldert@gmail.com>
2016-02-16 11:03:54 +0100

    minor changes to tagmanage

--- Mythe_2D/Assets/Scripts/Tags.cs
+++ Mythe_2D/Assets/Scripts/Tags.cs
@@ -3,5 +3,6 @@ using System.Collections;
 
 public static class Tags {
 
-    public static readonly string PLAYER = "Player";
+    public static readonly string playerTag = "Player";
+	public static readonly string enemyTag = "Enemy";
 }
