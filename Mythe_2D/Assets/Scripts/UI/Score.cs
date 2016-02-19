2a31bdfe4a833ad18a168359d4eb8353d22358f4
bennie12345 <bmuijldert@gmail.com>
2016-02-19 18:48:39 +0100

    added more placeholders and built prototype scene further

---
+++ Mythe_2D/Assets/Scripts/UI/Score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    private float _score = 0;
+    [SerializeField]private Text _scoreText;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void UpdateScore(float pointsAdded)
+    {
+        _score += pointsAdded;
+        _scoreText.text = " " + _score;
+    }
+}
