8e8e86ab5cd298d18ff04cc0ac4ef9ecd23b168a
Lorenzo <18927@ma-web.nl>
2016-02-25 11:10:36 +0100

    added stone enemy + enemy rip rotation
    :baby_chick:

---
+++ Mythe_2D/Assets/Scripts/Enemy/StoneEnemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneEnemy : MonoBehaviour {
+
+	void Update () {
+        rotateEnemy(); 
+    }
+
+    void rotateEnemy()
+    {
+        transform.Rotate(0, 0, -1);
+    }
+}
