a30402e4e50b44cd8afd880e13c45748c86b383d
bennie12345 <bmuijldert@gmail.com>
2016-02-27 00:55:23 +0100

    fixed memory leak (hopefully)

--- Mythe_2D/Assets/Scripts/Classes/DestroyObject.cs
+++ Mythe_2D/Assets/Scripts/Classes/DestroyObject.cs
@@ -5,26 +5,20 @@ public class DestroyObject : MonoBehaviour {
 
     [SerializeField]private float _destroyTime;
     private ObjectPool _objectPoolScript;
-    private bool _canPutBack = false;
 
 	void Start () 
     {
         _objectPoolScript = GameObject.FindWithTag(Tags.objectPoolTag).GetComponent<ObjectPool>();
 	}
 
-    void Update()
+    void OnEnable()
     {
-        if (!_canPutBack)
-        {
-            StartCoroutine(DestroyTheObject());
-        }
+        StartCoroutine(DestroyTheObject());
     }
 
     IEnumerator DestroyTheObject()
     {
-        _canPutBack = true;
         yield return new WaitForSeconds(_destroyTime);
         _objectPoolScript.PoolObject(this.gameObject);
-        _canPutBack = false;
     }
 }
