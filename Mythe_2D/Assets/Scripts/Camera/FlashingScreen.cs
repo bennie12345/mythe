6fdd245c42588792f43b17ba792b30b946e7b92d
Lorenzo <18927@ma-web.nl>
2016-03-10 10:08:29 +0100

    red flashing screen on hit ayy lmao

--- Mythe_2D/Assets/Scripts/Camera/FlashingScreen.cs
+++ Mythe_2D/Assets/Scripts/Camera/FlashingScreen.cs
@@ -4,35 +4,38 @@ using UnityEngine.UI;
 
 public class FlashingScreen : MonoBehaviour {
 
-    [SerializeField]private Image flashingImage;
     [SerializeField]private float fadeSpeed;
     private Color startingColor;
     private bool fade = false;
+    [SerializeField]private Image flashingImage;
+    private Color red;
 
-	void Start () {
-        //flashingImage.rectTransform.localScale = new Vector2(Screen.width, Screen.height);
-        flashingImage.enabled = true;
-        startingColor = flashingImage.color;
+    void Start () {
+        red = flashingImage.color;
+        startingColor = Color.clear;
+        flashingImage.color = startingColor;
     }
 	
 	void Update () {
-        if(flashingImage.color == Color.clear)
-        {
-            flashingImage.color = startingColor;
-            fade = false;
-        }
-        if (fade)
-        {
-            FadeToClear();
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            fade = true;
-        }
+        FadeToClear();
 	}
 
     void FadeToClear()
     {
-        flashingImage.color = Color.Lerp(flashingImage.color, Color.clear, fadeSpeed * Time.deltaTime);
+        if (fade == true && flashingImage.color != startingColor)
+        {
+            flashingImage.color = Color.Lerp(flashingImage.color, Color.clear, fadeSpeed * Time.deltaTime);
+        }
+
+        if(flashingImage.color == startingColor)
+        {
+            fade = false;
+        }
+    }
+
+    public void StartFade()
+    {
+        flashingImage.color = red;
+        fade = true;
     }
 }
