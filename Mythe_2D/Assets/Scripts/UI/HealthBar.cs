bc72bc5e099eb0b8316dd859ba348d5422123323
bennie12345 <bmuijldert@gmail.com>
2016-03-14 10:53:53 +0100

    implemented UI

--- Mythe_2D/Assets/Scripts/UI/HealthBar.cs
+++ Mythe_2D/Assets/Scripts/UI/HealthBar.cs
@@ -5,20 +5,23 @@ using System.Collections;
 public class HealthBar : MonoBehaviour {
 
     [SerializeField]private Image _healthbar;
-    private float _healthbarOffset;
+    [SerializeField]private Image _wing;
     private Player _playerScript;
+    private float _healthOffset = 1;
 
-	void Start () {
+	void Start () 
+    {
         _playerScript = GameObject.FindWithTag(Tags.playerTag).GetComponent<Player>();
-        _healthbarOffset = _playerScript.Health;
 	}
 	
-	void Update () {
+	void Update () 
+    {
         UpdateHealthbar();
 	}
 
     private void UpdateHealthbar()
     {
-        _healthbar.fillAmount = _playerScript.Health / _healthbarOffset;
+        _healthbar.fillAmount = _playerScript.Health - _healthOffset;
+        _wing.fillAmount = _playerScript.Health - _healthOffset;
     }
 }
