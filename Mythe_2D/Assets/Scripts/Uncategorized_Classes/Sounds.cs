510e5fd680ede41a90e547221be83d292a8dd0eb
bennie12345 <bmuijldert@gmail.com>
2016-03-25 19:02:33 +0100

    added fishsliced enemydeath

---
+++ Mythe_2D/Assets/Scripts/Uncategorized_Classes/Sounds.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sounds : MonoBehaviour {
+
+    [SerializeField]private AudioClip _medusaSound;
+    public AudioClip MedusaSound
+    {
+        get
+        {
+            return _medusaSound;
+        }
+        set
+        {
+            _medusaSound = value;
+        }
+    }
+    [SerializeField]private AudioClip _firingMahLazor;
+    public AudioClip FiringMahLazor
+    {
+        get
+        {
+            return _firingMahLazor;
+        }
+        set
+        {
+            _firingMahLazor = value;
+        }
+    }
+
+	void Start () {
+        this.gameObject.tag = Tags.soundsObjectTag;
+	}
+}
---
+++ Mythe_2D/Assets/Scripts/Uncategorized_Classes/Tags.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Tags {
+
+    public static readonly string playerTag = "Player";
+	public static readonly string birdEnemyTag = "BirdEnemy";
+    public static readonly string mainCameraTag = "Main Camera";
+    public static readonly string abilityButtonsTag = "AbilityButtons";
+    public static readonly string swordTag = "Sword";
+    public static readonly string laserTag = "Laser";
+    public static readonly string abilities = "Ability";
+    public static readonly string soundsObjectTag = "SoundsObject";
+    public static readonly string UITag = "UITag";
+    public static readonly string currentScoreTag = "CurrentScoreObject";
+    public static readonly string medusaTag = "MedusaAbility";
+    public static readonly string fishEnemyTag = "FishEnemy";
+    public static readonly string objectPoolTag = "ObjectPool";
+    public static readonly string slicedBirdOneTag = "SlicedBirdOne";
+    public static readonly string slicedBirdTwoTag = "SlicedBirdTwo";
+    public static readonly string flashingScreenObjectTag = "FlashingScreenObject";
+}
